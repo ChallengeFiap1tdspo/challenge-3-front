dcda95a098a9d8f3eeb2da994b054d24173a2272
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-11-02 13:28:28 -0300

    Implementando onSubmit: ativa isLoading, limpa apiError, loga dados e mapeia payload pacienteParaApi com sanitização do CPF

--- challenge/src/routes/Cadastro/index.tsx
+++ challenge/src/routes/Cadastro/index.tsx
@@ -23,4 +23,18 @@ export default function Cadastro() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [apiError, setApiError] = useState<string | null>(null);
+
+  const onSubmit = async (data: CadastroForm) => {
+    setIsLoading(true);
+    setApiError(null);
+    console.log("Tentando cadastrar:", data);
+ 
+    const pacienteParaApi = {
+        nome: data.nome,
+        idade: Number(data.idade),
+        tipoDeficiencia: data.tipoDeficiencia || null,
+        telefone: data.telefone || null,
+        cpf: data.cpf.replaceAll(/\D/g, ''),
+        email: data.email
+    };
  
