ef8d30ae56bb2f7c8364d763b387847f8b6e910a
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-11-09 15:16:52 -0300

    realese: ajuste chave api

--- challenge/.vercel/output/static/assets/index-D6chj5GX.js
+++ challenge/.vercel/output/static/assets/index-D6chj5GX.js
@@ -20638,7 +20638,7 @@ function Login() {
   ) });
 }
 const API_URL = "https://challenge-4-java.onrender.com";
-const API_KEY = undefined = "chave_secreta_muito_segura_123456";
+const API_KEY = "chave_secreta_muito_segura_123456";
 function Cadastro() {
   const {
     register,
