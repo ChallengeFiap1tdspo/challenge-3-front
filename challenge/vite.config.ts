50c2a2907731add90aa0b5424b188ec44ec38a95
phestalise <phestalise@gmail.com>
2025-11-09 14:05:27 -0300

    realese: ajustando config

--- challenge/vite.config.ts
+++ challenge/vite.config.ts
@@ -3,17 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', 
+  base: '/',
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-
-    sourcemap: true,
-    minify: 'esbuild'
-  },
- 
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    minify: false, 
   }
 })
