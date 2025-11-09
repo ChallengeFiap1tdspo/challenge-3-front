4e2d641f9f502f6878d6bda2f7a020abee4dcaa0
phestalise <phestalise@gmail.com>
2025-11-09 16:19:31 -0300

    feat:ajustando caminhos

--- challenge/vite.config.ts
+++ challenge/vite.config.ts
@@ -3,9 +3,22 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
     minify: false, 
+    sourcemap: true, 
+    rollupOptions: {
+      output: {
+    
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  },
+  server: {
+    port: 5173
   }
 })
