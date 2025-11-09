45cc3000a1ac1127ca0148043f5462f99096146e
phestalise <phestalise@gmail.com>
2025-11-09 13:33:39 -0300

    realese: ajustando config pra tentar subir pra vercel

--- challenge/vite.config.ts
+++ challenge/vite.config.ts
@@ -1,37 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/', 
-  plugins: [
-    react(),       
-    tailwindcss()  
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist',       
-    emptyOutDir: true,   
-    sourcemap: false,   
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-      
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  server: {
-    port: 5173,      
-    open: true,        
-    strictPort: true  
-  },
-  resolve: {
-    alias: {
-      '@': '/src'      
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
-});
+})
