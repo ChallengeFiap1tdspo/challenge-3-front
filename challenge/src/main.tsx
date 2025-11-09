86a65c08302d5d8e42cfd52da6413322ebea41ca
phestalise <phestalise@gmail.com>
2025-11-09 13:54:22 -0300

    realese: ajustando main

--- challenge/src/main.tsx
+++ challenge/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./global.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home/index.tsx";
 import Faq from "./routes/Faq/index.tsx";
 import Contato from "./routes/Contato/index.tsx";
@@ -14,27 +14,24 @@ import Sobre from "./routes/Sobre/index.tsx";
 import Login from "./routes/Login/index.tsx";
 import Cadastro from "./routes/Cadastro/index.tsx";
 import PrimeiroContato from "./routes/Primeiro-Contato/index.tsx";
-import EditarPerfil from "./routes/EditarPerfil/index.tsx"; 
+import EditarPerfil from "./routes/EditarPerfil/index.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
     errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/faq", element: <Faq /> },
-      { path: "/contato", element: <Contato /> },
-      { path: "/ajuda", element: <Ajuda /> },
-      { path: "/ajuda/:id", element: <Ajuda /> },
-      { path: "/equipe", element: <Equipe /> },
-      { path: "/sobre", element: <Sobre /> },
-      { path: "/login", element: <Login /> },
-      { path: "/cadastro", element: <Cadastro /> },
-      { path: "/primeiro-contato", element: <PrimeiroContato /> },
-      { path: "/editar-perfil", element: <EditarPerfil /> }
-    ],
   },
+  { path: "/faq", element: <Faq /> },
+  { path: "/contato", element: <Contato /> },
+  { path: "/ajuda", element: <Ajuda /> },
+  { path: "/ajuda/:id", element: <Ajuda /> },
+  { path: "/equipe", element: <Equipe /> },
+  { path: "/sobre", element: <Sobre /> },
+  { path: "/login", element: <Login /> },
+  { path: "/cadastro", element: <Cadastro /> },
+  { path: "/primeiro-contato", element: <PrimeiroContato /> },
+  { path: "/editar-perfil", element: <EditarPerfil /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
